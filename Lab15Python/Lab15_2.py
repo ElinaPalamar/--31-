22c16b25dd2776d7a72b21f216956ee24a11886d
Elina <elina.palamar@gmail.com>
2024-11-13 11:03:53 +0200

    Update Lab15_2.py

--- Lab15Python/Lab15_2.py
+++ Lab15Python/Lab15_2.py
@@ -2,19 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv')
-
 df['Date'] = pd.to_datetime(df['Date'])
 
 df_2014 = df[df['Date'].dt.year == 2014]
-
 df_2014['Month'] = df_2014['Date'].dt.month
 
+# Розрахунок сумарних даних по місяцях
 monthly_data = df_2014.drop(columns=['Date']).sum(axis=1).groupby(df_2014['Month']).sum()
 
+# Знаходження найбільш популярного місяця
 most_popular_month = monthly_data.idxmax()
-print(f"Найбільш популярний місяць: {most_popular_month}")
+month_names = {1: 'Січень', 2: 'Лютий', 3: 'Березень', 4: 'Квітень', 5: 'Травень',
+               6: 'Червень', 7: 'Липень', 8: 'Серпень', 9: 'Вересень', 10: 'Жовтень',
+               11: 'Листопад', 12: 'Грудень'}
+most_popular_month_name = month_names[most_popular_month]
 
-plt.figure(figsize=(10,6))
+print(f"Найбільш популярний місяць: {most_popular_month_name}")
+
+plt.figure(figsize=(10, 6))
 monthly_data.plot(kind='bar', color='skyblue')
 plt.title('Використання велодоріжок у 2014 році')
 plt.xlabel('Місяць')
